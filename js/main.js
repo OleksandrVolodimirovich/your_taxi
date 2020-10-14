4ffb9c83edf1f75525a23cf21e81a108f70f1404
OleksandrVolodimirovich <61995509+OleksandrVolodimirovich@users.noreply.github.com>
2020-10-14 23:40:14 +0300

    @media screen | all page + menu burger

--- js/main.js
+++ js/main.js
@@ -11,3 +11,14 @@ $('select').jselector();
 $(function () {
     $('.fancy').fancybox();
 });
+
+    $('.menu_toggle').click(function () {
+        if ($(this).hasClass('active')) {
+            $('.main_menu nav').slideUp();
+            $(this).removeClass('active');
+        }
+        else {
+            $('.main_menu nav').slideDown();
+            $(this).addClass('active');
+        }
+    });
